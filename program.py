6f7ad90758eb7d98c2ccb006ca655f0f49e2333b
Alexia Gallegos <Brocolash@MacBook-Pro-de-Rodrigo-494.local>
2022-02-13 01:21:35 -0600

    katas subidas de la 0 a la 1

---
+++ program.py
@@ -0,0 +1,22 @@
+from datetime import date
+# program.py
+sum = 1 + 2
+print (sum)
+print ('Hola desde la consola')
+
+print(type (sum))
+print(type('Hola desde la consola'))
+
+print(date.today())
+print("La fecha de hoy es " +  str (date.today()))
+
+# name = input("Quiúbole, ¿Cómo te llamas?\n")
+# print("Qué tranza, "+ name + " ¿Cómo estás?")
+
+
+
+print('\nSuma')
+pn = input('Introduce tu edad\n')
+sn = input('introduce tu peso\n')
+print ("La suma de tu edad mas tu peso es igual a: " + str(int(pn) + int(sn)))
+
